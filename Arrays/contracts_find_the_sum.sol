391571a35fda2f671562a389d98d8ac866eb1114
Natnael Tesfahun <nattytesfa4@gmail.com>
2026-05-24 14:42:33 +0300

    Add initial implementation of Contract with sum function for five numbers

---
+++ Arrays/contracts_find_the_sum.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Contract {
+    function sum(uint[5] memory _numbers) external pure returns(uint) {
+        uint total = 0;
+        for(uint i = 0; i < 5; i++) {
+            total += _numbers[i];
+        }
+        return total;
+    }
+}
