0b0448e45d7b40c5c24b47866b99a1f876cde8d0
Natnael Tesfahun <nattytesfa4@gmail.com>
2026-05-24 15:07:34 +0300

    Add Voting contract with proposal struct and new proposal functionality

---
+++ Voting/contracts_proposals.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+    }
+    
+    Proposal[] public proposals;
+    
+    function newProposal(address _target, bytes calldata _data) external {
+        proposals.push(Proposal({
+            target: _target,
+            data: _data,
+            yesCount: 0,
+            noCount: 0
+        }));
+    }
+}
