768f53eb8c5ffdaf32c67756128b7e20f69adde3
Bemnet Esayas <bemnetesayas167@gmail.com>
2026-05-26 19:14:06 +0300

    removed folder

--- Natnael Tesfahun/Functions_and_value_types/contracts_double_uint.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Contract {
-    function double(uint _value) external pure returns(uint) {
-        return _value * 2;
-    }
-}
--- Natnael Tesfahun/reverting_transactions/contracts_require_1_ether.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Contract {
-    constructor() payable {
-        require(msg.value >= 1 ether, "Must send at least 1 ether");
-    }
-}
